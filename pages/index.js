5bfebfe403da678c2682f293613e4678bf06fa04
spekboomh-prog <spekboomh@gmail.com>
2025-12-03 10:53:43 +0200

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 40 }}>
+      <h1 style={{ fontSize: 32, marginBottom: 20 }}>Welcome to Camperpixelz</h1>
+      <p>Your outdoor and camping brand is now LIVE in development!</p>
+      <p>More features and design are coming soon.</p>
+
+      <h3 style={{ marginTop: 30 }}>Follow us:</h3>
+      <ul>
+        <li>Facebook: <a href="https://facebook.com/camperpixelz" target="_blank">facebook.com/camperpixelz</a></li>
+        <li>Instagram: <a href="https://instagram.com/camperpixelz" target="_blank">instagram.com/camperpixelz</a></li>
+        <li>Email: <a href="mailto:hello@camperpixelz.com">hello@camperpixelz.com</a></li>
+      </ul>
+    </div>
+  );
+}
