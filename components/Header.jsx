0ddb4a27726f709b739f42e875340e5335da83db
spekboomh-prog <spekboomh@gmail.com>
2025-12-03 10:55:59 +0200

    Create components/Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header style={{
+      padding: "20px",
+      background: "#F6B042",
+      color: "#062F4F",
+      fontFamily: "Arial",
+      fontWeight: "bold",
+      fontSize: "22px"
+    }}>
+      camperpixelz
+    </header>
+  );
+}
